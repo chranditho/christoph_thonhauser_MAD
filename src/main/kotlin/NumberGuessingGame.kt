fe9b1fbe726fa0c29931e68efc8beacba109ebbb
Christoph <christoph.thonhauser@squer.at>
2023-03-05 20:42:44 +0100

    implement guessing game

--- src/main/kotlin/NumberGuessingGame.kt
+++ src/main/kotlin/NumberGuessingGame.kt
@@ -1,15 +1,29 @@
+import util.countSharedDigits
+import util.countSharedDigitsAtSamePosition
 import util.generateFourDigitNumber
 
 class NumberGuessingGame {
 
     companion object {
         fun start() {
+            val numberToGuess = generateFourDigitNumber()
+
             println("Welcome to the Number Guessing Game!")
 
-            // print("Please input your first 4-digit number guess:   ")
+            var guess = 0
 
-            val numberToGuess = generateFourDigitNumber()
-            println("This is the number to guess:  $numberToGuess")
+            while (guess != numberToGuess) {
+                print("Please input a 4-digit number guess:   ")
+                val input = readlnOrNull()
+                guess = input?.toInt()!!
+                println()
+                val n = countSharedDigits(guess, numberToGuess)
+                val m = countSharedDigitsAtSamePosition(guess, numberToGuess)
+                if (m != 4) println("Try again: $n:$m")
+            }
+
+            println("Congratulations, you have won!")
+            println("You have found the correct number $numberToGuess.")
         }
     }
 }
