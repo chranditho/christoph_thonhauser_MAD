3d71ae9907a275641be799ddd77cfe1dc0d9b2b5
Christoph <christoph.thonhauser@squer.at>
2023-03-05 21:41:33 +0100

    simplify generating random number

--- src/main/kotlin/util/NumberGenerator.kt
+++ src/main/kotlin/util/NumberGenerator.kt
@@ -1,23 +1,14 @@
 package util
 
 fun generateFourDigitNumberWithUniqueDigits(): Int {
-    val oncePlace = (Math.random() * 10).toInt()
+    val digits = mutableListOf<Int>()
 
-    var tensPlace = (Math.random() * 10).toInt()
-    while (oncePlace == tensPlace)
-        tensPlace = (Math.random() * 10).toInt()
+    while (digits.size < 4) {
+        val digit = (0..9).random()
+        if (!digits.contains(digit)) {
+            digits.add(digit)
+        }
+    }
 
-    var hundredsPlace = (Math.random() * 10).toInt()
-    while (hundredsPlace == oncePlace || hundredsPlace == tensPlace)
-        hundredsPlace = (Math.random() * 10).toInt()
-
-    var thousandsPlace = (Math.random() * 10).toInt()
-    while (thousandsPlace == oncePlace || thousandsPlace == tensPlace || thousandsPlace == hundredsPlace || thousandsPlace == 0)
-        thousandsPlace = (Math.random() * 10).toInt()
-
-    tensPlace *= 10
-    hundredsPlace *= 100
-    thousandsPlace *= 1000
-
-    return oncePlace + tensPlace + hundredsPlace + thousandsPlace
+    return digits[0] * 1000 + digits[1] * 100 + digits[2] * 10 + digits[3]
 }
