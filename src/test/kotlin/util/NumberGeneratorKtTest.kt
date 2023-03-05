d0e09bcbc5b530b7ff863462beb45399c8d37886
Christoph <christoph.thonhauser@squer.at>
2023-03-05 15:39:00 +0100

    test if number does not contain repeating digits

--- src/test/kotlin/util/NumberGeneratorKtTest.kt
+++ src/test/kotlin/util/NumberGeneratorKtTest.kt
@@ -9,4 +9,19 @@ class NumberGeneratorKtTest {
     fun `ensure number consists of four digits`() {
         Assertions.assertEquals(generateFourDigitNumber().toString().length, 4)
     }
+
+    @Test
+    fun `ensure number does not contain repeating digits`() {
+        val digits = generateFourDigitNumber().toString().toCharArray()
+        val uniqueDigits = HashSet<Char>()
+        var isUnique = true
+
+        for (digit in digits) {
+            if (!uniqueDigits.add(digit)) {
+                isUnique = false
+            }
+        }
+
+        Assertions.assertTrue(isUnique)
+    }
 }
