4c640e5695e754c9323b21b8c6cf0191f7a157eb
Christoph <christoph.thonhauser@squer.at>
2023-03-05 14:58:31 +0100

    make number unique

--- src/main/kotlin/NumberGuessingGame.kt
+++ src/main/kotlin/NumberGuessingGame.kt
@@ -2,9 +2,22 @@ class NumberGuessingGame {
 
     private fun generateFourDigitNumber(): Number {
         val oncePlace = (Math.random() * 10).toInt()
-        val tensPlace = (Math.random() * 10).toInt() * 10
-        val hundredsPlace = (Math.random() * 10).toInt() * 100
-        val thousandsPlace = (Math.random() * 10).toInt() * 1000
+
+        var tensPlace = (Math.random() * 10).toInt()
+        while (oncePlace == tensPlace)
+            tensPlace = (Math.random() * 10).toInt()
+
+        var hundredsPlace = (Math.random() * 10).toInt()
+        while (hundredsPlace == oncePlace || hundredsPlace == tensPlace)
+            hundredsPlace = (Math.random() * 10).toInt()
+
+        var thousandsPlace = (Math.random() * 10).toInt()
+        while (thousandsPlace == oncePlace || thousandsPlace == tensPlace || thousandsPlace == hundredsPlace)
+            thousandsPlace = (Math.random() * 10).toInt()
+
+        tensPlace *= 10
+        hundredsPlace *= 100
+        thousandsPlace *= 1000
 
         return oncePlace + tensPlace + hundredsPlace + thousandsPlace
     }
