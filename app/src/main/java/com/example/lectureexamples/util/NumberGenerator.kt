59623c9c36474a9a1d457329a66b16f477163c0d
Christoph <christoph.thonhauser@squer.at>
2023-03-18 18:40:52 +0100

    introduce android app lecture examples

---
+++ app/src/main/java/com/example/lectureexamples/util/NumberGenerator.kt
@@ -0,0 +1,14 @@
+package com.example.lectureexamples.util
+
+fun generateFourDigitNumberWithUniqueDigits(): Int {
+    val digits = mutableListOf<Int>()
+
+    while (digits.size < 4) {
+        val digit = (0..9).random()
+        if (!digits.contains(digit)) {
+            digits.add(digit)
+        }
+    }
+
+    return digits[0] * 1000 + digits[1] * 100 + digits[2] * 10 + digits[3]
+}
---
+++ app/src/test/java/com/example/lectureexamples/util/NumberGeneratorKtTest.kt
@@ -0,0 +1,26 @@
+package com.example.lectureexamples.util
+
+import org.junit.Test
+
+class NumberGeneratorKtTest {
+
+    @Test
+    fun `ensure number consists of four digits`() {
+       // Assertions.assertEquals(4, generateFourDigitNumberWithUniqueDigits().toString().length)
+    }
+
+    @Test
+    fun `ensure number does not contain repeating digits`() {
+        val digits = generateFourDigitNumberWithUniqueDigits().toString().toCharArray()
+        val uniqueDigits = HashSet<Char>()
+        var isUnique = true
+
+        for (digit in digits) {
+            if (!uniqueDigits.add(digit)) {
+                isUnique = false
+            }
+        }
+
+      //  Assertions.assertTrue(isUnique)
+    }
+}
