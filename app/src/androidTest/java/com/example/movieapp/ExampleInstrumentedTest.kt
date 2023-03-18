4b51baa31df44ceeeef3c5189928c4618ccb2a1b
Christoph <christoph.thonhauser@squer.at>
2023-03-18 19:33:22 +0100

    fix setup

--- app/src/androidTest/java/com/example/movieapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/movieapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.lectureexamples", appContext.packageName)
+        assertEquals("com.example.movieapp", appContext.packageName)
     }
 }
