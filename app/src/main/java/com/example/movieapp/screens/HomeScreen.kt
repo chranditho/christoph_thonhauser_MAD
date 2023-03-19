c588265cd73223b339ad97061ced38cd15584f0b
Christoph <christoph.thonhauser@squer.at>
2023-03-19 19:28:24 +0100

    Add add clickable row

--- app/src/main/java/com/example/movieapp/screens/HomeScreen.kt
+++ app/src/main/java/com/example/movieapp/screens/HomeScreen.kt
@@ -4,18 +4,40 @@ import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -42,28 +64,26 @@ fun HomeScreen(navController: NavController) {
             Greeting()
             Text(
                 style = MaterialTheme.typography.h6,
-                text= "Movie List"
+                text = "Movie List"
             )
             MyList(navController)
         }
-        //MyList()
-        //Greeting()
-        //WelcomeText(modifier = Modifier.padding(16.dp), text = "welcome to my app!")
     }
 }
 
 
 @Preview
 @Composable
-fun MyList(navController: NavController = rememberNavController(),
-           movies: List<Movie> = getMovies()){
-    LazyColumn{
-        items(movies) {movie ->
+fun MyList(
+    navController: NavController = rememberNavController(),
+    movies: List<Movie> = getMovies()
+) {
+    LazyColumn {
+        items(movies) { movie ->
             MovieRow(
                 movie = movie,
-            )  { movieId ->
+            ) { movieId ->
                 Log.d("MyList", "item clicked $movieId")
-                // navigate to detailscreen
                 navController.navigate("detail/$movieId")
             }
         }
@@ -96,17 +116,20 @@ fun TopAppBar(title: String = "Movie App") {
 
 @Composable
 fun MovieRow(movie: Movie, onItemClick: (String) -> Unit = {}) {
-    Card(modifier = Modifier
-        .fillMaxWidth()
-        .padding(5.dp)
-        .clickable { onItemClick(movie.id) },
+    var expanded by remember { mutableStateOf(false) }
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp)
+            .clickable { onItemClick(movie.id) },
         shape = RoundedCornerShape(corner = CornerSize(15.dp)),
         elevation = 5.dp
     ) {
         Column {
-            Box(modifier = Modifier
-                .height(150.dp)
-                .fillMaxWidth()
+            Box(
+                modifier = Modifier
+                    .height(150.dp)
+                    .fillMaxWidth()
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.avatar2),
@@ -114,27 +137,43 @@ fun MovieRow(movie: Movie, onItemClick: (String) -> Unit = {}) {
                     contentScale = ContentScale.Crop
                 )
 
-                Box(modifier = Modifier
-                    .fillMaxSize()
-                    .padding(10.dp),
-                    contentAlignment = Alignment.TopEnd
-                ){
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(10.dp),
+                    contentAlignment = Alignment.TopEnd,
+                ) {
                     Icon(
                         tint = MaterialTheme.colors.secondary,
                         imageVector = Icons.Default.FavoriteBorder,
-                        contentDescription = "Add to favorites")
+                        contentDescription = "Add to favorites"
+                    )
                 }
             }
 
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(5.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
+            Row(
+                modifier = Modifier
+                    .clickable(onClick = { expanded = !expanded })
+                    .fillMaxWidth()
+                    .padding(5.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+
+                ) {
                 Text(movie.title, style = MaterialTheme.typography.h6)
                 Icon(
                     imageVector = Icons.Default.KeyboardArrowUp,
-                    contentDescription = "Show details")
+                    contentDescription = "Show details"
+                )
+            }
+            if (expanded) {
+                Box(
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .fillMaxWidth()
+                        .clickable(onClick = { expanded = !expanded })
+                ) {
+                    Text(movie.actors, style = MaterialTheme.typography.body2)
+                }
             }
         }
     }
@@ -168,8 +207,9 @@ fun Greeting() {
         OutlinedTextField(
             value = name,
             onValueChange = {
-                name = it},
-            label = { Text("Name")}
+                name = it
+            },
+            label = { Text("Name") }
         )
 
 
@@ -186,7 +226,6 @@ fun Greeting() {
             label = { Text("Name")}
         )
         */
-
 
 
         /*
