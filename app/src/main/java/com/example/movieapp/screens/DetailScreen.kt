5864029a1f83a2111824bd652741689ddd028489
Christoph <christoph.thonhauser@squer.at>
2023-03-26 23:48:29 +0200

    add movie images row

--- app/src/main/java/com/example/movieapp/screens/DetailScreen.kt
+++ app/src/main/java/com/example/movieapp/screens/DetailScreen.kt
@@ -2,13 +2,25 @@ package com.example.movieapp.screens
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.example.movieapp.models.Movie
 import com.example.movieapp.models.getMovies
 import com.example.movieapp.widgets.MovieRow
@@ -32,8 +44,39 @@ fun DetailScreen(navController: NavController, movieId: String?) {
                 navController = navController
             )
             MovieRow(movie = movie)
+            MovieLazyRow(movieImages = movie.images)
 
+        }
+    }
+}
 
+@Composable
+fun MovieLazyRow(movieImages: List<String>) {
+
+    LazyRow(modifier = Modifier.fillMaxWidth()) {
+        itemsIndexed(movieImages) { index, image ->
+            Card(
+                modifier = Modifier
+                    .width(120.dp)
+                    .height(180.dp)
+                    .padding(horizontal = 8.dp),
+                elevation = 8.dp
+            ) {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Image(
+                        painter = rememberImagePainter(
+                            data = image,
+                            builder = {
+                                crossfade(true)
+                            }
+                        ),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .align(Alignment.Center)
+                    )
+                }
+            }
         }
     }
 }
