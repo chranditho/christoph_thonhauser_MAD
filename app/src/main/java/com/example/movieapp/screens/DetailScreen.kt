3d2a831e1443923501e25f208e806c254ffd5320
Christoph <christoph.thonhauser@squer.at>
2023-03-26 13:15:46 +0200

    add topbar with movie  title to detail screen

--- app/src/main/java/com/example/movieapp/screens/DetailScreen.kt
+++ app/src/main/java/com/example/movieapp/screens/DetailScreen.kt
@@ -1,16 +1,16 @@
 package com.example.movieapp.screens
 
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import com.example.movieapp.models.Movie
+import com.example.movieapp.models.getMovies
+import com.example.movieapp.widgets.TopAppBar
 
 @Composable
 fun DetailScreen(navController: NavController, movieId: String?) {
+    val movie: Movie = getMovies().filter { it.id == movieId }[0]
 
-    movieId?.let {
-        Text(text = "Hello Detailscreen $movieId")
-    }
-
+    TopAppBar(title = movie.title)
 
 
 }
