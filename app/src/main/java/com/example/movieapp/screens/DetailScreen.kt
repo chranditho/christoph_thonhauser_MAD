5aefcf83e8542704d6bccd1b0eea68da0da369a9
Christoph <christoph.thonhauser@squer.at>
2023-03-26 13:35:56 +0200

    show movie row

--- app/src/main/java/com/example/movieapp/screens/DetailScreen.kt
+++ app/src/main/java/com/example/movieapp/screens/DetailScreen.kt
@@ -1,16 +1,39 @@
 package com.example.movieapp.screens
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import com.example.movieapp.models.Movie
 import com.example.movieapp.models.getMovies
+import com.example.movieapp.widgets.MovieRow
 import com.example.movieapp.widgets.TopAppBar
 
+@RequiresApi(Build.VERSION_CODES.Q)
 @Composable
 fun DetailScreen(navController: NavController, movieId: String?) {
     val movie: Movie = getMovies().filter { it.id == movieId }[0]
 
-    TopAppBar(title = movie.title, showDropDownMenu = false, showBackButton = true, navController = navController)
+    Surface(
+        modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
+    ) {
+
+        Column {
+
+            TopAppBar(
+                title = movie.title,
+                showDropDownMenu = false,
+                showBackButton = true,
+                navController = navController
+            )
+            MovieRow(movie = movie)
 
 
+        }
+    }
 }
